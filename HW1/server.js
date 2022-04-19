5304be3aa7bbf43ebb8df06f33b95e29181a8727
Joako-Sador <jfernandezvizcarra@gmail.com>
2022-04-19 00:42:42 -0400

    first commit uwu

---
+++ HW1/server.js
@@ -0,0 +1,30 @@
+/* IMPORTS */
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+
+//-------------------------------------------
+
+/* CONFIGS */
+
+const server = express()
+dotenv.config()
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+server.use(bodyParser.json())
+server.use(cors())
+
+/* VARIABLES */
+
+var port = process.env.PORT || 3000
+var ip = process.env.PORT || 'localhost'
+
+server.get('/', (req,res) => res.send('<h1>Server Operando</h1>'))
+
+/* PORTS */
+
+server.listen(port,()=>{
+    console.log(`Servidor de grpc-app corriendo en: http://${ip}:${port}.`)
+})
